0f6ee0a98cd917f85db878be0fe8c2380ac6ef66
PedrohsResende <117690162+PedrohsResende@users.noreply.github.com>
2022-11-07 23:51:06 -0300

    Update svm lccv.py

--- svm lccv.py
+++ svm lccv.py
@@ -1,20 +1,20 @@
-import matplotlib.pyplot as plt #plotar gráficos
-from sklearn import datasets, svm, metrics  #a própria svm para o dataset Digits
+import matplotlib.pyplot as plt #plotar gráficos e mostrar resultados
+from sklearn import datasets, svm, metrics  #a própria svm para o dataset Digits, e metrics para ajudar a mostrar na tela
 import pickle #salvar no disco e usar depois
 
 def trainSVM():
     #carregar base de dados
     digits = datasets.load_digits()
 
-    #transformar as imagens 8x8 em vetor de 64 posições
+    #transformar cada imagem 8x8 em vetor de 64 posições
     numberOFSamples = len(digits.images)
     data = digits.images.reshape((numberOFSamples, -1))
     print(data.shape) # número de 1797 imagens 8x8
 
     #criar o classificador
-    classificador = svm.SVC(gamma = "scale")
+    classificador = svm.SVC(gamma = "scale") #gamma ajusta a kernel
 
-    #treinar o clasificador com 50% das imagens na base de dados
+    #o fit faz o treinamento do clasificador com 50% das imagens na base de dados
     classificador.fit(data[:numberOFSamples // 2 ], digits.target[:numberOFSamples // 2])
 
     #salvar no disco para não precisar treinar a cada teste
@@ -35,22 +35,21 @@ def loadSVM():
         classificador = pickle.load(PonteiroDisco)
 
     #predizer os rótulos da segunda metade das imagens no banco de dados
-    rotulosEsperados = digits.target[numberOFSamples // 2 :]
-    rotulosPreditos = classificador.predict(data[numberOFSamples // 2 :])
+    respostaEsperada = digits.target[numberOFSamples // 2 :]
+    respostaPredita = classificador.predict(data[numberOFSamples // 2 :])
 
     #matrizes de confusão
-    print(classificador, metrics.classification_report(rotulosEsperados, rotulosPreditos))
-    print(metrics.confusion_matrix(rotulosEsperados, rotulosPreditos))
+    print(classificador, metrics.classification_report(respostaEsperada, respostaPredita))
+    print(metrics.confusion_matrix(respostaEsperada, respostaPredita))
 
     #exibir 12 imagens do dataset com as classificações
-    listaImagensPredicoes = list(zip(digits.images[numberOFSamples // 2 :], rotulosPreditos))
-    for index, (images, predicao) in enumerate(listaImagensPredicoes[:12:]):
+    listaImagensPredicoes = list(zip(digits.images[numberOFSamples // 2 :], respostaPredita, respostaEsperada))
+    for index, (images, predicao, esperada) in enumerate(listaImagensPredicoes[:12:]):
         print(index)
         plt.subplot(3, 4, index + 1)
         plt.axis('off')
         plt.imshow(images, interpolation='nearest')
-        plt.title("Predicao: %i" % predicao)
-
+        plt.title("Predicao: {}, Esperada: {}".format(predicao,esperada))
     plt.show()
         
 if __name__ == "__main__":
